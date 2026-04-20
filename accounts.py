5528a895bfe4aa252547c5c8e16bd97481307117
Dogen <setratosfer@gmail.com>
2026-04-21 06:20:42 +0700

    Create accounts.py

---
+++ accounts.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCOUNTS = []
+
+for key, value in os.environ.items():
+    if key.startswith("KEY_") and value:
+        ACCOUNTS.append({
+            "name": key,
+            "key": value
+        })
